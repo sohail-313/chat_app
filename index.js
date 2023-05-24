aa039c128560a3c5c571d2e99d3ce2f8aceb46d2
Mohammed Sohail <85786620+sohail-313@users.noreply.github.com>
2023-05-24 10:49:41 +0530

    server listen canged

--- index.js
+++ index.js
@@ -36,7 +36,6 @@ io.on('connection', (socket) =>{
 
 
 
-
-server.listen(3001, ()=>{
-    console.log("Server Started")
-})
+server.listen(process.env.PORT || 3001, () => {
+    console.log("Server Started");
+});
